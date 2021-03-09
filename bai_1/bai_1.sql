0333ccced2540e65e57c8aad93cab0f41d86518e
truongthieuhuyen <nguyentientruong97@gmail.com>
2021-03-10 02:00:26 +0700

    nop bai 2

--- bai_1/bai_1.sql
+++ bai_1/bai_1.sql
@@ -1,3 +1,30 @@
 #CREATE SCHEMA `student_cms_plusplus` DEFAULT CHARACTER SET utf8mb4 ;
 
-drop table `student_class_dele`;
+CREATE TABLE `student_cms_plusplus`.`class` (
+  `class_id` int NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(250) DEFAULT NULL,
+  `major` varchar(250) DEFAULT NULL,
+  `total_student` int DEFAULT NULL,
+  `teacher_name` varchar(250) DEFAULT NULL,
+  `teacher_phone` int DEFAULT NULL,
+  `created` timestamp(6) NULL DEFAULT NULL,
+  `last_updated` timestamp(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`class_id`)
+);
+CREATE TABLE `student` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `mssv` varchar(12) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `phone` int DEFAULT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `created` timestamp(6) NULL DEFAULT NULL,
+  `last_updated` timestamp(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
+);
+CREATE TABLE `student_class` (
+    `student_id` INT DEFAULT NULL,
+    `class_id` INT DEFAULT NULL
+)
